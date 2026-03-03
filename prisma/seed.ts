fabcda17db0083c67e73c84150e585987213cc77
JorgeMarles <jamarlesf@gmail.com>
2026-03-02 20:35:01 -0500

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,14 @@
 import { PrismaClient, Role } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const prisma = new PrismaClient();
 
 async function main() {
-    const adminEmail = 'admin@sprintfit.com';
-    const adminPassword = 'adminPassword123'; // Cambia esto en producción
+    const adminEmail = process.env.ADMIN_EMAIL || 'admin@sprintfit.com';
+    const adminPassword = process.env.ADMIN_PASSWORD || 'adminPassword123';
 
     const existingAdmin = await prisma.user.findUnique({
         where: { email: adminEmail },
